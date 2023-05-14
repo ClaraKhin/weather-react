35cfd2d3d5e8eb7f98669d94cee189de1d192ff9
ClaraKhin <khinthirikyaw8@gmail.com>
2023-05-14 21:29:36 +0630

    include react loader spinner and animated weather for better interface

--- src/Weather.js
+++ src/Weather.js
@@ -1,5 +1,6 @@
 import React from "react";
 import axios from "axios";
+import { Oval } from "react-loader-spinner";
 
 export default function Weather() {
   function handleResponse(response) {
@@ -9,8 +10,17 @@ export default function Weather() {
   let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=New York&appid=${apiKey}&units=metric`;
   axios.get(apiUrl).then(handleResponse);
   return (
-    <div>
-      <h3>Weather!</h3>
-    </div>
+    <Oval
+      height={100}
+      width={100}
+      color="#4fa94d"
+      wrapperStyle={{}}
+      wrapperClass=""
+      visible={true}
+      ariaLabel="oval-loading"
+      secondaryColor="#4fa94d"
+      strokeWidth={2}
+      strokeWidthSecondary={2}
+    />
   );
 }
