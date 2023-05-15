6651ae127f6cb665185c64d6bf169148fb1b05ed
ClaraKhin <khinthirikyaw8@gmail.com>
2023-05-15 09:22:23 +0630

    Access the Open Weather API to display weather

--- src/Weather.js
+++ src/Weather.js
@@ -1,26 +1,60 @@
-import React from "react";
+import React, { useState } from "react";
 import axios from "axios";
-import { Oval } from "react-loader-spinner";
+import "./Weather.css";
 
 export default function Weather() {
-  function handleResponse(response) {
+  const [city, setCity] = useState("");
+  const [loaded, setLoaded] = useState(false);
+  const [weather, setWeather] = useState({});
+
+  function displayWeather(response) {
+    setLoaded(true);
     console.log(response.data);
+    setWeather({
+      temperature: response.data.main.temp,
+      wind: response.data.wind.speed,
+      humidity: response.data.main.humidity,
+      icon: `https://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`,
+      description: response.data.weather[0].description,
+    });
   }
-  let apiKey = "4e982f559f14469b2722f0f59015fdae";
-  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=New York&appid=${apiKey}&units=metric`;
-  axios.get(apiUrl).then(handleResponse);
-  return (
-    <Oval
-      height={100}
-      width={100}
-      color="#4fa94d"
-      wrapperStyle={{}}
-      wrapperClass=""
-      visible={true}
-      ariaLabel="oval-loading"
-      secondaryColor="#4fa94d"
-      strokeWidth={2}
-      strokeWidthSecondary={2}
-    />
+
+  function handleSubmit(event) {
+    event.preventDefault();
+    const apiKey = "4e982f559f14469b2722f0f59015fdae";
+    const apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+    axios.get(apiUrl).then(displayWeather);
+  }
+
+  function updateCity(event) {
+    setCity(event.target.value);
+  }
+
+  const form = (
+    <form onSubmit={handleSubmit}>
+      <input type="search" placeholder="Enter a City" onChange={updateCity} />
+      <button type="Submit">Search</button>
+    </form>
   );
+
+  if (loaded) {
+    return (
+      <div className="Container">
+        {form}
+        <ul>
+          <li>Temperature: {Math.round(weather.temperature)}°C</li>
+          <li>Wind: {weather.wind}km/h </li>
+
+          <li>Humidity: {weather.humidity}% </li>
+
+          <li>
+            <img src="weather.icon" alt={weather.description} />
+          </li>
+          <li>Description: {weather.description} </li>
+        </ul>
+      </div>
+    );
+  } else {
+    return form;
+  }
 }
