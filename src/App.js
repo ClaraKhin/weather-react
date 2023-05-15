219cafb01ffb496aeb9a97310bd74bfb6d1a262f
ClaraKhin <khinthirikyaw8@gmail.com>
2023-05-15 11:55:46 +0630

    included source codes and fixed style

--- src/App.js
+++ src/App.js
@@ -6,8 +6,31 @@ import "./App.css";
 function App() {
   return (
     <div className="App">
-      <h1>Check The Weather</h1>
-      <Weather />
+      <div className="Container">
+        <h1>Check The Weather</h1>
+        <Weather />
+      </div>
+
+      <p>
+        Coded By
+        <a
+          href="https://github.com/ClaraKhin"
+          className="Address"
+          target="blank"
+        >
+          {" "}
+          Khin
+        </a>{" "}
+      </p>
+      <p>
+        <a
+          href="https://github.com/ClaraKhin/weather-react"
+          className="SourceCode"
+          target="blank"
+        >
+          Source Code
+        </a>
+      </p>
     </div>
   );
 }
